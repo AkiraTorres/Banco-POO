3ef0dba302646ed2bc52ac014cf37dc998a880b8
AkiraTorres <thiago.torress@upe.br>
2023-06-28 11:47:47 -0300

    fix: Investimento agora herda de ServicoBancario

--- Investimento.py
+++ Investimento.py
@@ -1,7 +1,7 @@
-from abc import ABC
+from ServicoBancario import ServicoBancario
 from Cliente import Cliente
 
-class Investimento(ABC):
+class Investimento(ServicoBancario):
     def __init__(self, cliente: Cliente, saldo: float = 0.0) -> None:
         super().__init__(cliente, saldo)
 
